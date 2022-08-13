6fb1c094b0b9568055a7c17da85581284a8443c4
JeysonZG <jdzg1124@gmail.com>
2022-08-13 11:01:03 -0500

    Cambio en el main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Probando trabajo en equipo");
+        System.out.println("Nuevo cambio - JeysonZG");
+
     }
 }
